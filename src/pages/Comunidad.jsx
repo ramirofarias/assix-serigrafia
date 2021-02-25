5716d35c6135e2de215a54b08e4b995ff9a886a4
Ramiro Farias <ramirofarias.dev@gmail.com>
2021-02-25 17:01:34 -0300

    Añadido imágenes a comunidad, y fondo a videos

--- src/pages/Comunidad.jsx
+++ src/pages/Comunidad.jsx
@@ -1,7 +1,16 @@
-import { Card, CardContent, Grid } from "@material-ui/core";
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Grid,
+} from "@material-ui/core";
 import React from "react";
 import styled from "styled-components";
-
+import "./Comunidad.css";
+import Telegram from "./img/telegram.png";
+import Facebook from "./img/facebook.png";
+import Instagram from "./img/instagram.png";
 const Titulo = styled.h2`
   color: #232323;
 `;
@@ -14,40 +23,60 @@ const Comunidad = () => {
   return (
     <Grid
       container
-      direction="column"
+      direction="row"
       alignItems="center"
       style={{ height: "100vh" }}
     >
-      <Grid item>
-        <Titulo>COMUNIDAD</Titulo>
-      </Grid>
-      <Grid item>
-        <Texto>🤔 Resolvé todas tus dudas,</Texto>
-        <Texto>👕 Compartí tus creaciones,</Texto>
-        <Texto>🤙 Y mucho más!</Texto>
+      <Grid container direction="column" justify="center" alignItems="center">
+        <Grid item>
+          <Titulo>COMUNIDAD</Titulo>
+        </Grid>
+        <Grid item>
+          <Texto>🤔 Resolvé todas tus dudas,</Texto>
+          <Texto>👕 Compartí tus creaciones,</Texto>
+          <Texto>🤙 Y mucho más!</Texto>
+        </Grid>
       </Grid>
       <Grid container direction="row" justify="center">
         <Grid item>
-          <Card
-            style={{ backgroundColor: "#232323" }}
-            id="viewsCount"
-            className="card"
-          >
+          <Card style={{ backgroundColor: "#232323" }} className="card">
+            <CardMedia
+              component="img"
+              alt="Canal de Telegram"
+              height="200"
+              image={Telegram}
+              title="Canal de Telegram"
+            />
             <CardContent>
-              <h2></h2>
-              <h2>visualizaciones</h2>
+              <h4>Canal de Telegram</h4>
             </CardContent>
           </Card>
         </Grid>
         <Grid item>
-          <Card
-            style={{ backgroundColor: "#232323" }}
-            id="viewsCount"
-            className="card"
-          >
+          <Card style={{ backgroundColor: "#232323" }} className="card">
+            <CardMedia
+              component="img"
+              alt="Grupo de Facebook"
+              height="200"
+              image={Facebook}
+              title="Grupo de Facebook"
+            />
             <CardContent>
-              <h2></h2>
-              <h2>visualizaciones</h2>
+              <h4>Grupo de Facebook</h4>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item>
+          <Card style={{ backgroundColor: "#232323" }} className="card">
+            <CardMedia
+              component="img"
+              alt="Perfil en Instagram"
+              height="200"
+              image={Instagram}
+              title="Perfil en Instagram"
+            />
+            <CardContent>
+              <h4>Perfil en Instagram</h4>
             </CardContent>
           </Card>
         </Grid>
